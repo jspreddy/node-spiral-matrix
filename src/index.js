fe69fc668050a9d93336679980719a5fddfc8c39
Sai Jonnala <sai.jonnala@passportinc.com>
2019-12-24 13:09:05 -0500

    spiral matrix works !!!!!!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,147 @@
-export function one() {
-  return false;
+
+function directLog(...params) {
+  process.stdout.write(params.toString());
 }
 
-export function two() { }
+// const log = directLog;
+const { log } = console;
+
+export function initEmptySquareMatrix(size) {
+  const matrix = [];
+  if (size <= 0) return undefined;
+  for (let x = 0; x < size; x += 1) {
+    const row = [];
+    for (let y = 0; y < size; y += 1) {
+      row.push(null);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
+
+export function incrementalMatrix(size) {
+  const matrix = initEmptySquareMatrix(size);
+  let number = 0;
+
+  for (let x = 0; x < size; x += 1) {
+    for (let y = 0; y < size; y += 1) {
+      number += 1;
+      matrix[x][y] = number;
+    }
+  }
+
+  return matrix;
+}
+
+
+export function spiralMatrix(size) {
+  const matrix = initEmptySquareMatrix(size);
+  let x = 0;
+  let y = 0;
+
+  let direction = 'right';
+
+  function currentIndex() {
+    return { x, y };
+  }
+
+  function goToNext() {
+    switch (direction) {
+      default:
+      case 'right': {
+        const nextx = x + 1;
+        if (nextx < size && matrix[y][nextx] == null) {
+          x = nextx;
+          return { x, y };
+        }
+        if ((nextx < size && matrix[y][nextx] !== null) || nextx === size) {
+          direction = 'down';
+          return goToNext();
+        }
+
+        break;
+      }
+
+      case 'down': {
+        const nexty = y + 1;
+        if (nexty < size && matrix[nexty][x] == null) {
+          y = nexty;
+          return { x, y };
+        }
+
+        if ((nexty < size && matrix[nexty][x] !== null) || nexty === size) {
+          direction = 'left';
+          return goToNext();
+        }
+
+        break;
+      }
+
+      case 'left': {
+        const prevx = x - 1;
+        if (prevx >= 0 && matrix[y][prevx] == null) {
+          x = prevx;
+          return { x, y };
+        }
+
+        if ((prevx >= 0 && matrix[y][prevx] !== null) || prevx < 0) {
+          direction = 'up';
+          return goToNext();
+        }
+
+        break;
+      }
+      case 'up': {
+        const prevy = y - 1;
+        if (prevy >= 0 && matrix[prevy][x] == null) {
+          y = prevy;
+          return { x, y };
+        }
+
+        if ((prevy >= 0 && matrix[prevy][x] !== null) || prevy < 0) {
+          direction = 'right';
+          return goToNext();
+        }
+
+        break;
+      }
+    }
+
+    return { x, y };
+  }
+
+  function getCurrentValue() {
+    return matrix[y][x];
+  }
+
+  function putCurrentValue(val) {
+    matrix[y][x] = val;
+  }
+
+  function getMatrix() {
+    return matrix;
+  }
+
+  function fillIncrementalIntegers() {
+    const length = size * size;
+
+    let i = 1;
+    putCurrentValue(i);
+    while (i < length) {
+      i += 1;
+      goToNext();
+      putCurrentValue(i);
+    }
+
+    return getMatrix();
+  }
+
+  return {
+    currentIndex,
+    goToNext,
+    getCurrentValue,
+    putCurrentValue,
+    getMatrix,
+    fillIncrementalIntegers,
+  };
+}
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,19 +1,193 @@
 import {
-  one,
-  two,
+  spiralMatrix,
+  initEmptySquareMatrix,
+  incrementalMatrix,
 } from '../index';
 
 
 describe('index.js', () => {
-  describe('one', () => {
-    it('should be a function with proper signature', async () => {
-      expect(typeof one).toEqual('function');
+  describe('initSquareMatrix', () => {
+    it('should return undefined for size 0', async () => {
+      expect(initEmptySquareMatrix(0)).toEqual(undefined);
+    });
+    it('should return proper matrix for size 1', async () => {
+      expect(initEmptySquareMatrix(1)).toEqual([
+        [null],
+      ]);
+    });
+
+    it('should return proper matrix for size 2', async () => {
+      expect(initEmptySquareMatrix(2)).toEqual([
+        [null, null],
+        [null, null],
+      ]);
+    });
+
+    it('should return proper matrix for size 3', async () => {
+      expect(initEmptySquareMatrix(3)).toEqual([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ]);
     });
   });
 
-  describe('two', () => {
+  describe('incrementalMatrix', () => {
     it('should be a function with proper signature', async () => {
-      expect(typeof two).toEqual('function');
+      expect(typeof incrementalMatrix).toEqual('function');
+      expect(incrementalMatrix.length).toEqual(1);
+    });
+
+    it('should return a square matrix with values in a +1 sequence starting at 1', async () => {
+      const matrix = incrementalMatrix(4);
+      expect(matrix).toEqual([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+      ]);
+    });
+  });
+
+  describe('spiralMatrix', () => {
+    it('should be a function with proper signature', async () => {
+      expect(typeof spiralMatrix).toEqual('function');
+    });
+
+    it('should go in a circle if I am not setting values.', async () => {
+      const spiral = spiralMatrix(4);
+
+      // first loop on the outer rim of matrix.
+      expect(spiral.currentIndex()).toEqual({ x: 0, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 1 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 2 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 2 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 1 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 0 });
+
+
+      // second loop on the outer rim of matrix.
+      expect(spiral.currentIndex()).toEqual({ x: 0, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 0 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 1 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 2 });
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 3 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 2 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 1 });
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 0 });
+
+      // ten loops around the empty matrix's outer rim.
+      for (let i = 0; i < 10; i += 1) {
+        expect(spiral.currentIndex()).toEqual({ x: 0, y: 0 });
+        expect(spiral.goToNext()).toEqual({ x: 1, y: 0 });
+        expect(spiral.goToNext()).toEqual({ x: 2, y: 0 });
+        expect(spiral.goToNext()).toEqual({ x: 3, y: 0 });
+        expect(spiral.goToNext()).toEqual({ x: 3, y: 1 });
+        expect(spiral.goToNext()).toEqual({ x: 3, y: 2 });
+        expect(spiral.goToNext()).toEqual({ x: 3, y: 3 });
+        expect(spiral.goToNext()).toEqual({ x: 2, y: 3 });
+        expect(spiral.goToNext()).toEqual({ x: 1, y: 3 });
+        expect(spiral.goToNext()).toEqual({ x: 0, y: 3 });
+        expect(spiral.goToNext()).toEqual({ x: 0, y: 2 });
+        expect(spiral.goToNext()).toEqual({ x: 0, y: 1 });
+        expect(spiral.goToNext()).toEqual({ x: 0, y: 0 });
+      }
+    });
+
+    it('should form a spiral if I put values and then goToNext ', async () => {
+      const spiral = spiralMatrix(4);
+
+      expect(spiral.currentIndex()).toEqual({ x: 0, y: 0 });
+      spiral.putCurrentValue(1);
+
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 0 });
+      spiral.putCurrentValue(2);
+
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 0 });
+      spiral.putCurrentValue(3);
+
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 0 });
+      spiral.putCurrentValue(4);
+
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 1 });
+      spiral.putCurrentValue(5);
+
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 2 });
+      spiral.putCurrentValue(6);
+
+      expect(spiral.goToNext()).toEqual({ x: 3, y: 3 });
+      spiral.putCurrentValue(7);
+
+      expect(spiral.getMatrix()).toEqual([
+        [1, 2, 3, 4],
+        [null, null, null, 5],
+        [null, null, null, 6],
+        [null, null, null, 7],
+      ]);
+
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 3 });
+      spiral.putCurrentValue(8);
+
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 3 });
+      spiral.putCurrentValue(9);
+
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 3 });
+      spiral.putCurrentValue(10);
+
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 2 });
+      spiral.putCurrentValue(11);
+
+      expect(spiral.goToNext()).toEqual({ x: 0, y: 1 });
+      spiral.putCurrentValue(12);
+
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 1 });
+      spiral.putCurrentValue(13);
+
+      expect(spiral.getMatrix()).toEqual([
+        [1, 2, 3, 4],
+        [12, 13, null, 5],
+        [11, null, null, 6],
+        [10, 9, 8, 7],
+      ]);
+
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 1 });
+      spiral.putCurrentValue(14);
+
+      expect(spiral.goToNext()).toEqual({ x: 2, y: 2 });
+      spiral.putCurrentValue(15);
+
+      expect(spiral.goToNext()).toEqual({ x: 1, y: 2 });
+      spiral.putCurrentValue(16);
+
+      expect(spiral.getMatrix()).toEqual([
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7],
+      ]);
+    });
+
+    it('should create a spiral matrix of size 5', async () => {
+      const spiral = spiralMatrix(5);
+      expect(spiral.fillIncrementalIntegers()).toEqual([
+        [1, 2, 3, 4, 5],
+        [16, 17, 18, 19, 6],
+        [15, 24, 25, 20, 7],
+        [14, 23, 22, 21, 8],
+        [13, 12, 11, 10, 9],
+      ]);
     });
   });
 });
